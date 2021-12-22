bee212ec32eecb413a0a22332b612316d7421ba1
Galdino <mateusgaldino1000@hotmail.com>
2021-12-22 15:38:33 -0300

    Implement MirageJs and react-query to mock back-end api calls

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,26 @@
-import { ChakraProvider } from "@chakra-ui/react"
-import { AppProps } from "next/app"
-import { NavDrawerProvider } from "../contexts/NavDrawerContext"
-import { theme } from "../styles/theme"
+import { ChakraProvider } from "@chakra-ui/react";
+import { AppProps } from "next/app";
+import { NavDrawerProvider } from "../contexts/NavDrawerContext";
+import { makeServer } from "../services/mirage";
+import { theme } from "../styles/theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <NavDrawerProvider>
-        <Component {...pageProps} />
-      </NavDrawerProvider>
+      <QueryClientProvider client={queryClient}>
+        <NavDrawerProvider>
+          <Component {...pageProps} />
+        </NavDrawerProvider>
+      </QueryClientProvider>
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,28 +1,57 @@
-import { Box, Button, Checkbox, Flex, Icon, Table, Tbody, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Icon,
+  Spinner,
+  Table,
+  Tbody,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import Link from "next/link";
+import { useEffect } from "react";
 import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { HeadingComponent } from "../../components/Heading";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import { User } from "../../components/UserList"
+import { User } from "../../components/UserList";
+import { useQuery } from "react-query";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery("users", async () => {
+    const response = await fetch("http://localhost:3000/api/users");
+    const data = await response.json();
+    
+    const users = data.users.map(user => (
+      {
+        id:user.id,
+        name: user.name,
+        email: user.email,
+        createdAt : new Date(user.createdAt).toLocaleDateString("pt-br", {
+          day: "2-digit",
+          month: "long",
+          year: "numeric"
+        })
+      }
+    ))
+    return users;
+  });
 
   const isWideVersion = useBreakpointValue({
     base: false,
-    lg: true
-  })
+    lg: true,
+  });
 
   return (
     <Box>
       <Header />
-      <Flex
-        w="100%"
-        my="6"
-        maxW={1480}
-        mx="auto"
-        px="6">
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
         <Sidebar />
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
@@ -39,38 +68,44 @@ export default function UserList() {
               </Button>
             </Link>
           </Flex>
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th w="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <User
-                userName="Mateus Galdino"
-                email="galdino_mateus1000@outlook.com"
-                date="04 de Abril, 2021"
-                isWideVersion={isWideVersion} />
-              <User
-                userName="Mateus Galdino"
-                email="galdino_mateus1000@outlook.com"
-                date="04 de Abril, 2021"
-                isWideVersion={isWideVersion} />
-              <User
-                userName="Mateus Galdino"
-                email="galdino_mateus1000@outlook.com"
-                date="04 de Abril, 2021"
-                isWideVersion={isWideVersion} />
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justifyContent="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justifyContent="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th w="8"></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map((user) => (
+                    <User
+                      userName={user.name}
+                      email={user.email}
+                      date={user.createdAt}
+                      isWideVersion={isWideVersion}
+                      key={user.id}
+                    />
+                  ))}
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
-      </Flex >
+      </Flex>
     </Box>
-  )
+  );
 }
